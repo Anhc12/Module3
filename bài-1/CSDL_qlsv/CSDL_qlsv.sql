01c55f9a15145725377be09b6c71af7acb3e64a7
Hoang Anh <anhnh265@gmail.com>
2025-03-17 01:08:25 +0700

    add CSDL_qlsv

---
+++ bài-1/CSDL_qlsv/CSDL_qlsv.sql
@@ -0,0 +1,20 @@
+create database demo1;
+use demo1;
+drop table sinhvien;
+create table student(
+id int,
+ten nvarchar(255),
+age int , 
+country varchar(50)
+);
+create table class (
+id int ,
+name varchar(50)
+);
+create table teacher (
+id int ,
+name varchar (50),
+age int ,
+country varchar(50)
+);
+ select 
